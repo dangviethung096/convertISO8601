ee9a57b1d8a2521a7c52ada0122074ad070fbd7d
Đặng Việt Hưng <dangviethung096@gmail.com>
2021-03-01 10:59:27 +0700

    Initial commit

---
+++ src/vitcon/lib/convertiso8601/Main.java
@@ -0,0 +1,83 @@
+package vitcon.lib.convertiso8601;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        String time = "2021-02-26T08:30:52.542-02:00";
+        Calendar dateTime = convertIso8601(time);
+        if (dateTime != null) {
+            System.out.println(dateTime.getTime().toString());
+        }
+
+        System.out.println(TimeZone.getDefault().getDisplayName());
+    }
+
+    /**
+     * convert string (with iso8601 time format) to calendar
+     * @param iso8601Time
+     * @return
+     */
+    public static Calendar convertIso8601(String iso8601Time) {
+
+        Calendar dateTime = null;
+        try {
+            // Check format iso 8601 without timezone
+            if (iso8601Time.matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{0,10}[\\dZ:+-]+")) {
+                // Init variable
+                dateTime = Calendar.getInstance();
+                // Get date and time
+                int year = Integer.parseInt(iso8601Time.substring(0, 4));
+                int month = Integer.parseInt(iso8601Time.substring(5, 7));
+                int day = Integer.parseInt(iso8601Time.substring(8, 10));
+                int hour = Integer.parseInt(iso8601Time.substring(11, 13));
+                int minute = Integer.parseInt(iso8601Time.substring(14, 16));
+                int second = Integer.parseInt(iso8601Time.substring(17, 19));
+                // Set value
+                dateTime.set(year, month - 1, day, hour, minute, second);
+
+                // Time zone
+                // Cut year, month, day
+                iso8601Time = iso8601Time.substring(iso8601Time.indexOf('T') + 1);
+                // Init time zone
+                TimeZone timeZone = null;
+                // check time zone
+                if (iso8601Time.contains("Z")) {
+                    // Get time zone
+                    timeZone = TimeZone.getTimeZone(ZoneOffset.ofHoursMinutes(0, 0));
+                    dateTime.setTimeZone(timeZone);
+                } else if (iso8601Time.contains("+") | iso8601Time.contains("-")) {
+                    // Create timezone
+                    int tzMinute = Integer.parseInt(iso8601Time.substring(iso8601Time.lastIndexOf(':') + 1));
+                    // + or -
+                    if (iso8601Time.contains("+")) {
+                        int tzHour = Integer.parseInt(iso8601Time.substring(iso8601Time.indexOf('+') + 1, iso8601Time.lastIndexOf(':')));
+                        timeZone = TimeZone.getTimeZone(ZoneOffset.ofHoursMinutes(tzHour, tzMinute));
+                    } else {
+                        int tzHour = Integer.parseInt(iso8601Time.substring(iso8601Time.indexOf('-') + 1, iso8601Time.lastIndexOf(':')));
+                        timeZone = TimeZone.getTimeZone(ZoneOffset.ofHoursMinutes(-tzHour, -tzMinute));
+                    }
+
+                    dateTime.setTimeZone(timeZone);
+                }
+            }
+
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        // Return value
+        return dateTime;
+    }
+
+    
+}
